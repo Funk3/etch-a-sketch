75788b2f17091fa36b3071acc56a286553063658
Zak <zak.johansson@gmail.com>
2022-02-11 15:30:54 -0800

    added reload function

--- javascript.js
+++ javascript.js
@@ -1,20 +1,31 @@
 const boxHolder = document.querySelector('.boxHolder');
 
-
 //create 256 boxes and insert them into boxholder class
-for (i = 0; i < 256; i++) {
-    const boxes = document.createElement('div');
-    boxes.classList.add('box');
-    boxHolder.appendChild(boxes);
-};
-
-function changeColor() {
-
+function makeGrid(){
+    for (i = 0; i < 256; i++) {
+        const boxes = document.createElement('div');
+        boxes.classList.add('box')
+        boxHolder.appendChild(boxes);
+        boxes.addEventListener('mouseover', function (e) {
+            e.target.style.background = "black";
+        });
+    }
 }
 
-boxHolder.addEventListener('onmouseover', changeColor());
+makeGrid();
+
+const boxRef = Array.from(document.getElementsByClassName('box'));
+const reload = document.querySelector('.reloading');
+
+reload.innerHTML= "Reload";
+reload.addEventListener('click', reload => {
+    boxRef.forEach((box) => {
+        box.removeAttribute('style')
+    })
+});
 
 
+ 
 
 
 
