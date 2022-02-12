8f4c11cc5af755e8e9f5c4eaa8ce2bb61c3e8268
Zak <zak.johansson@gmail.com>
2022-02-11 19:18:31 -0800

    started 'change size' button functionality

--- javascript.js
+++ javascript.js
@@ -1,5 +1,10 @@
 const boxHolder = document.querySelector('.boxHolder');
+const size = document.querySelector('.amount');
 
+let gridSize = prompt("Please choose a number between 1-100!");
+
+size.innerHTML = "Change Size";
+size.addEventListener('click', makeGrid, false);
 //create 256 boxes and insert them into boxholder class
 function makeGrid(){
     for (i = 0; i < 256; i++) {
@@ -9,7 +14,7 @@ function makeGrid(){
         boxes.addEventListener('mouseover', function (e) {
             e.target.style.background = "black";
         });
-    }
+    } 
 }
 
 makeGrid();
@@ -17,13 +22,15 @@ makeGrid();
 const boxRef = Array.from(document.getElementsByClassName('box'));
 const reload = document.querySelector('.reloading');
 
-reload.innerHTML= "Reload";
+reload.innerHTML= "Clear";
 reload.addEventListener('click', reload => {
     boxRef.forEach((box) => {
         box.removeAttribute('style')
     })
 });
 
+
+console.log(gridSize);
 
  
 
